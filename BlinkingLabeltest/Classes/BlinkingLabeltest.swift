6edd6deffb4018b6c8ff5c84e09d27a2a875f070
Dhekra Rouatbi <dhekra.rouatbi@proxym-it.com>
2017-07-12 12:58:37 +0100

    commit 5.0

--- BlinkingLabeltest/Classes/BlinkingLabeltest.swift
+++ BlinkingLabeltest/Classes/BlinkingLabeltest.swift
@@ -8,22 +8,17 @@
 
 import UIKit
 
-open class BlinkingLabel : UILabel {
+public class BlinkingLabeltest: UILabel {
+
     
-    /**
-     Tells the label to start blinking.
-     */
-    open func startBlinking() {
-        let options : UIViewAnimationOptions = .Repeat | .Autoreverse
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = .repeat
         UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
             self.alpha = 0
         }, completion: nil)
     }
     
-    /**
-     Tells the label to stop blinking.
-     */
-    open func stopBlinking() {
+    public func stopBlinking() {
         alpha = 1
         layer.removeAllAnimations()
     }
